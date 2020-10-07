f67417d2a47475e2fcb9a03426f04dbb6f6de7c5
cristinashuw <cristinashuw@gmail.com>
2020-10-07 15:07:19 +0700

    Declaration Preferences.java

--- app/src/main/java/com/cristinashuw/simplelogin/Preferences.java
+++ app/src/main/java/com/cristinashuw/simplelogin/Preferences.java
@@ -32,5 +32,49 @@ public class Preferences {
 
     /** Deklarasi Edit Preferences dan mengubah data
      *  yang memiliki key KEY_PASS_TEREGISTER dengan parameter password */
+    public static void setRegisteredPass(Context context, String password){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putString(KEY_PASS_TEREGISTER, password);
+        editor.apply();
+    }
+
+    /** Mengembalikan nilai dari key KEY_PASS_TEREGISTER berupa String */
+    public static String getRegisteredPass(Context context){
+        return getSharedPreferences(context).getString(KEY_PASS_TEREGISTER,"");
+    }
+
+    /** Deklarasi Edit Preferences dan mengubah data
+     *  yang memiliki key KEY_USERNAME_SEDANG_LOGIN dengan parameter username */
+    public static void setLoggedInUser(Context context, String username){
+            SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putString(KEY_USERNAME_SEDANG_LOGIN, username);
+        editor.apply();
+    }
+    /** Mengembalikan nilai dari key KEY_USERNAME_SEDANG_LOGIN berupa String */
+    public static String getLoggedInUser(Context context){
+        return getSharedPreferences(context).getString(KEY_USERNAME_SEDANG_LOGIN,"");
+    }
+
+    /** Deklarasi Edit Preferences dan mengubah data
+     *  yang memiliki key KEY_STATUS_SEDANG_LOGIN dengan parameter status */
+    public static void setLoggedInStatus(Context context, boolean status){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putBoolean(KEY_STATUS_SEDANG_LOGIN,status);
+        editor.apply();
+    }
+    /** Mengembalikan nilai dari key KEY_STATUS_SEDANG_LOGIN berupa boolean */
+    public static boolean getLoggedInStatus(Context context){
+        return getSharedPreferences(context).getBoolean(KEY_STATUS_SEDANG_LOGIN,false);
+    }
+
+    /** Deklarasi Edit Preferences dan menghapus data, sehingga menjadikannya bernilai default
+     *  khusus data yang memiliki key KEY_USERNAME_SEDANG_LOGIN dan KEY_STATUS_SEDANG_LOGIN */
+    public static void clearLoggedInUser (Context context){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.remove(KEY_USERNAME_SEDANG_LOGIN);
+        editor.remove(KEY_STATUS_SEDANG_LOGIN);
+        editor.apply();
+    }
+
 
 }
